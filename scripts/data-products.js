eea5af7d66cc20b7c87c2375e4a14799b9c096c5
hajar-elad <hajar.elad@gmail.com>
2025-11-30 22:15:04 +0100

    Add files via upload

---
+++ scripts/data-products.js
@@ -0,0 +1,218 @@
+export const products = [
+    {
+        id : 'id1',
+        name :'Adaptateur De Capteur Infrarouge',
+        image :'images/adaptateur-capteur-infrarouge.JPG',
+        price : 419
+    }
+    ,
+     {
+        id : 'id2',
+        name :'Mi Bedside Lamp 2',
+        image :'images/mi-bedside-lamp2.JPG',
+        price : 529
+    }
+    ,
+     {
+        id : 'id3',
+        name :'Mi Bluetooth Speaker',
+        image :'images/mi-bluetooth-speaker.JPG',
+        price : 319
+    }
+    ,
+     {
+        id : 'id4',
+        name :'Mi Box Projector2 Pro',
+        image :'images/mi-box-projector2-pro.JPG',
+        price : 9299
+    }
+    , 
+    {
+        id : 'id5',
+        name :'Mibro Lite Montre Homme',
+        image :'images/mibro-lite-montre-homme.JPG',
+        price : 599
+    }
+    ,
+     {
+        id : 'id6',
+        name :'Mibro Watch C2',
+        image :'images/mibro-watch-c2.JPG',
+        price : 349
+    }
+    ,
+     {
+        id : 'id7',
+        name :'Mi Compact Bluetooth',
+        image :'images/mi-compact-bluetooth.JPG',
+        price : 149
+    }
+    ,
+     {
+        id : 'id8',
+        name :'Mi Computer Glasses',
+        image :'images/mi-computer-glasses.JPG',
+        price : 179
+    }
+    ,
+     {
+        id : 'id9',
+        name :'Mi Lcd Writing Tablet 13,5',
+        image :'images/mi-lcd-writing-tablet-13,5.JPG',
+        price : 229
+    }
+    ,
+     {
+        id : 'id10',
+        name :'Mi Led Desk Lamp 1S',
+        image :'images/mi-led-desk-lamp-1S.JPG',
+        price : 569
+    }
+    ,
+     {
+        id : 'id11',
+        name :'Mi Portable Bluetooth Speaker',
+        image :'images/mi-portable-bluetooth-speaker.JPG',
+        price : 439
+    }
+    ,
+    {
+        id : 'id12',
+        name :'Mi Robot Vaccum Mop 2 Pro',
+        image :'images/mi-robot-vaccum-mop2-pro.JPG',
+        price : 3899
+    }
+    ,
+     {
+        id : 'id13',
+        name :'Mi Robot Vaccum Mop Blanc',
+        image :'images/mi-robot-vaccum-mop-blanc.JPG',
+        price : 2899
+    }
+    ,
+     {
+        id : 'id14',
+        name :'Mi Robot Vaccum Mop P',
+        image :'images/mi-robot-vaccum-mop-p.JPG',
+        price : 3199
+    }
+    ,
+     {
+        id : 'id15',
+        name :'Mi Smart Electric Folding Bike',
+        image :'images/mi-smart-electric-folding-bike.JPG',
+        price : 8999
+    }
+    , 
+    {
+        id : 'id16',
+        name :'Mi Smart Projector 2 EU',
+        image :'images/mi-smart-projector2-eu.JPG',
+        price : 8236
+    }
+    ,
+     {
+        id : 'id17',
+        name :'Mi Smart Standing Lamp',
+        image :'images/mi-smart-standing-lamp.JPG',
+        price : 699
+    }
+    ,
+     {
+        id : 'id18',
+        name :'Station De Charge Xiaomi',
+        image :'images/station-de-charge-xiaomi.JPG',
+        price : 219
+    }
+    ,
+     {
+        id : 'id19',
+        name :'Thermometre Frontal Landwind',
+        image :'images/thermometre-frontal-landwind.JPG',
+        price : 199
+    }
+    ,
+     {
+        id : 'id20',
+        name :'Xiaomi Camping Lantern',
+        image :'images/xiaomi-camping-lantern.JPG',
+        price : 699
+    }
+    ,
+     {
+        id : 'id21',
+        name :'Xiaomi City Backpack',
+        image :'images/xiaomi-city-backpack.JPG',
+        price : 499
+    }
+    ,
+     {
+        id : 'id22',
+        name :'Xiaomi Commuter Helmet',
+        image :'images/xiaomi-commuter-helmet.JPG',
+        price : 461
+    }
+    ,
+     {
+        id : 'id23',
+        name :'Xiaomi Electric Scooter Pneumatic',
+        image :'images/xiaomi-electric-scooter-pneumatic.JPG',
+        price : 379
+    }
+    ,
+     {
+        id : 'id24',
+        name :'Xiaomi Electric Scooter Riding Gloves',
+        image :'images/xiaomi-electric-scooter-riding-gloves.JPG',
+        price : 459
+    }
+    ,
+     {
+        id : 'id25',
+        name :'Xiaomi Electric Scooter Storage Bag',
+        image :'images/xiaomi-electric-scooter-storage-bag.JPG',
+        price : 473
+    }
+    ,
+     {
+        id : 'id26',
+        name :'Xiaomi Flashlight',
+        image :'images/xiaomi-flashlight.JPG',
+        price : 399
+    }
+    , 
+    {
+        id : 'id27',
+        name :'Xiaomi Smart Air Fryer 6.5L White',
+        image :'images/xiaomi-smart-air-fryer-6.5L-white.JPG',
+        price : 1649
+    }
+    ,
+     {
+        id : 'id28',
+        name :'Xiaomi Smart Camera C200',
+        image :'images/xiaomi-smart-camera-c200.JPG',
+        price : 359
+    }
+    ,
+     {
+        id : 'id29',
+        name :'Xiaomi TV Stick 4k EU TV Box',
+        image :'images/xiaomi-tv-stick-4k-eu-tv-box.JPG',
+        price : 679
+    }
+    ,
+     {
+        id : 'id30',
+        name :'Xiaomi Vaccum G11EU',
+        image :'images/xiaomi-vaccum G11EU.JPG',
+        price : 4499
+    }
+    ,
+     {
+        id : 'id31',
+        name :'Xiaomi Watch S1 Qctive Charging Dock',
+        image :'images/xiaomi-watch-s1-qctive-charging-dock.JPG',
+        price : 219
+    }
+]
---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+import {products} from '../scripts/data-products.js'
+
+function displayProducts(){
+
+    let html = '';
+    products.forEach((product) => {
+        html += `
+        <div class="product">
+           <img class="product-image" src= "${product.image}"/>
+           <div class="Product-infos">
+              <div class="product-name" >${product.name}</div>
+              <div class="product-price">${(product.price).toFixed(2)} MAD</div>
+           </div>
+           <a href="https://wa.me/212701131213" target="_blank" 
+                title="Nous sommes joignable sur whatsapp" class="order-link">
+               Commander 
+           </a>
+        </div>
+        `
+    })
+
+    document.querySelector('.products')
+      .innerHTML = html;
+      console.log(html);
+}
+
+displayProducts();
