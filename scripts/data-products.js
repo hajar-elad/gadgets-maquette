434a925d1ecece73733e19845e3e3fe57e554af7
hajar-elad <hajar.elad@gmail.com>
2025-12-01 19:24:33 +0100

    Add files via upload

--- scripts/data-products.js
+++ scripts/data-products.js
@@ -2,217 +2,217 @@ export const products = [
     {
         id : 'id1',
         name :'Adaptateur De Capteur Infrarouge',
-        image :'images/adaptateur-capteur-infrarouge.JPG',
+        image :'images/adaptateur-capteur-infrarouge.jpg',
         price : 419
     }
     ,
      {
         id : 'id2',
         name :'Mi Bedside Lamp 2',
-        image :'images/mi-bedside-lamp2.JPG',
+        image :'images/mi-bedside-lamp2.jpg',
         price : 529
     }
     ,
      {
         id : 'id3',
         name :'Mi Bluetooth Speaker',
-        image :'images/mi-bluetooth-speaker.JPG',
+        image :'images/mi-bluetooth-speaker.jpg',
         price : 319
     }
     ,
      {
         id : 'id4',
         name :'Mi Box Projector2 Pro',
-        image :'images/mi-box-projector2-pro.JPG',
+        image :'images/mi-box-projector2-pro.jpg',
         price : 9299
     }
     , 
     {
         id : 'id5',
         name :'Mibro Lite Montre Homme',
-        image :'images/mibro-lite-montre-homme.JPG',
+        image :'images/mibro-lite-montre-homme.jpg',
         price : 599
     }
     ,
      {
         id : 'id6',
         name :'Mibro Watch C2',
-        image :'images/mibro-watch-c2.JPG',
+        image :'images/mibro-watch-c2.jpg',
         price : 349
     }
     ,
      {
         id : 'id7',
         name :'Mi Compact Bluetooth',
-        image :'images/mi-compact-bluetooth.JPG',
+        image :'images/mi-compact-bluetooth.jpg',
         price : 149
     }
     ,
      {
         id : 'id8',
         name :'Mi Computer Glasses',
-        image :'images/mi-computer-glasses.JPG',
+        image :'images/mi-computer-glasses.jpg',
         price : 179
     }
     ,
      {
         id : 'id9',
         name :'Mi Lcd Writing Tablet 13,5',
-        image :'images/mi-lcd-writing-tablet-13,5.JPG',
+        image :'images/mi-lcd-writing-tablet-13,5.jpg',
         price : 229
     }
     ,
      {
         id : 'id10',
         name :'Mi Led Desk Lamp 1S',
-        image :'images/mi-led-desk-lamp-1S.JPG',
+        image :'images/mi-led-desk-lamp-1S.jpg',
         price : 569
     }
     ,
      {
         id : 'id11',
         name :'Mi Portable Bluetooth Speaker',
-        image :'images/mi-portable-bluetooth-speaker.JPG',
+        image :'images/mi-portable-bluetooth-speaker.jpg',
         price : 439
     }
     ,
     {
         id : 'id12',
         name :'Mi Robot Vaccum Mop 2 Pro',
-        image :'images/mi-robot-vaccum-mop2-pro.JPG',
+        image :'images/mi-robot-vaccum-mop2-pro.jpg',
         price : 3899
     }
     ,
      {
         id : 'id13',
         name :'Mi Robot Vaccum Mop Blanc',
-        image :'images/mi-robot-vaccum-mop-blanc.JPG',
+        image :'images/mi-robot-vaccum-mop-blanc.jpg',
         price : 2899
     }
     ,
      {
         id : 'id14',
         name :'Mi Robot Vaccum Mop P',
-        image :'images/mi-robot-vaccum-mop-p.JPG',
+        image :'images/mi-robot-vaccum-mop-p.jpg',
         price : 3199
     }
     ,
      {
         id : 'id15',
         name :'Mi Smart Electric Folding Bike',
-        image :'images/mi-smart-electric-folding-bike.JPG',
+        image :'images/mi-smart-electric-folding-bike.jpg',
         price : 8999
     }
     , 
     {
         id : 'id16',
         name :'Mi Smart Projector 2 EU',
-        image :'images/mi-smart-projector2-eu.JPG',
+        image :'images/mi-smart-projector2-eu.jpg',
         price : 8236
     }
     ,
      {
         id : 'id17',
         name :'Mi Smart Standing Lamp',
-        image :'images/mi-smart-standing-lamp.JPG',
+        image :'images/mi-smart-standing-lamp.jpg',
         price : 699
     }
     ,
      {
         id : 'id18',
         name :'Station De Charge Xiaomi',
-        image :'images/station-de-charge-xiaomi.JPG',
+        image :'images/station-de-charge-xiaomi.jpg',
         price : 219
     }
     ,
      {
         id : 'id19',
         name :'Thermometre Frontal Landwind',
-        image :'images/thermometre-frontal-landwind.JPG',
+        image :'images/thermometre-frontal-landwind.jpg',
         price : 199
     }
     ,
      {
         id : 'id20',
         name :'Xiaomi Camping Lantern',
-        image :'images/xiaomi-camping-lantern.JPG',
+        image :'images/xiaomi-camping-lantern.jpg',
         price : 699
     }
     ,
      {
         id : 'id21',
         name :'Xiaomi City Backpack',
-        image :'images/xiaomi-city-backpack.JPG',
+        image :'images/xiaomi-city-backpack.jpg',
         price : 499
     }
     ,
      {
         id : 'id22',
         name :'Xiaomi Commuter Helmet',
-        image :'images/xiaomi-commuter-helmet.JPG',
+        image :'images/xiaomi-commuter-helmet.jpg',
         price : 461
     }
     ,
      {
         id : 'id23',
         name :'Xiaomi Electric Scooter Pneumatic',
-        image :'images/xiaomi-electric-scooter-pneumatic.JPG',
+        image :'images/xiaomi-electric-scooter-pneumatic.jpg',
         price : 379
     }
     ,
      {
         id : 'id24',
         name :'Xiaomi Electric Scooter Riding Gloves',
-        image :'images/xiaomi-electric-scooter-riding-gloves.JPG',
+        image :'images/xiaomi-electric-scooter-riding-gloves.jpg',
         price : 459
     }
     ,
      {
         id : 'id25',
         name :'Xiaomi Electric Scooter Storage Bag',
-        image :'images/xiaomi-electric-scooter-storage-bag.JPG',
+        image :'images/xiaomi-electric-scooter-storage-bag.jpg',
         price : 473
     }
     ,
      {
         id : 'id26',
         name :'Xiaomi Flashlight',
-        image :'images/xiaomi-flashlight.JPG',
+        image :'images/xiaomi-flashlight.jpg',
         price : 399
     }
     , 
     {
         id : 'id27',
         name :'Xiaomi Smart Air Fryer 6.5L White',
-        image :'images/xiaomi-smart-air-fryer-6.5L-white.JPG',
+        image :'images/xiaomi-smart-air-fryer-6.5L-white.jpg',
         price : 1649
     }
     ,
      {
         id : 'id28',
         name :'Xiaomi Smart Camera C200',
-        image :'images/xiaomi-smart-camera-c200.JPG',
+        image :'images/xiaomi-smart-camera-c200.jpg',
         price : 359
     }
     ,
      {
         id : 'id29',
         name :'Xiaomi TV Stick 4k EU TV Box',
-        image :'images/xiaomi-tv-stick-4k-eu-tv-box.JPG',
+        image :'images/xiaomi-tv-stick-4k-eu-tv-box.jpg',
         price : 679
     }
     ,
      {
         id : 'id30',
         name :'Xiaomi Vaccum G11EU',
-        image :'images/xiaomi-vaccum G11EU.JPG',
+        image :'images/xiaomi-vaccum G11EU.jpg',
         price : 4499
     }
     ,
      {
         id : 'id31',
         name :'Xiaomi Watch S1 Qctive Charging Dock',
-        image :'images/xiaomi-watch-s1-qctive-charging-dock.JPG',
+        image :'images/xiaomi-watch-s1-qctive-charging-dock.jpg',
         price : 219
     }
 ]
